9fd9098674ae50f3e2a5030a62e7d208700e554a
codesky <super_codesky@163.com>
2019-01-27 13:55:01 +0800

    change: 增加视频上传activity

---
+++ app/src/main/java/cn/edu/bit/codesky/minidouyin/ui/VideoUploadActivity.java
@@ -0,0 +1,20 @@
+package cn.edu.bit.codesky.minidouyin.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.edu.bit.codesky.minidouyin.R;
+
+/**
+ * @author codesky
+ * @date 2019/1/27 13:53
+ * @description 视频上传activity
+ */
+public class VideoUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_upload);
+    }
+}
